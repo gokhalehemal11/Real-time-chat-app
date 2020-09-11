da2cc228cd955d3d89734c9c634e4459c269056a
Hemal Gokhale <31528745+gokhalehemal11@users.noreply.github.com>
2020-09-11 10:59:24 +0530

    Chat Sidebar and chat refresh ready

--- chat.js
+++ chat.js
@@ -1,6 +1,7 @@
 // Global variables to store the chat_history (past chats of user and logged in user email)
 var cur_email= "";
 var chat_history= []
+var total_chats=0;
 
 // Detect the change of user authentication state. Whether user is logged in or logged out
 auth.onAuthStateChanged(user => {
@@ -17,7 +18,7 @@ auth.onAuthStateChanged(user => {
 });
 
 
-// logout
+/*// logout
 const logout = document.querySelector('#logout');
 //console.log(logout)
 logout.addEventListener('click', (e) => {
@@ -78,7 +79,7 @@ groupChat.addEventListener('submit', (e) => {
   }).catch(function(error) {
     console.error('Error writing new message to database', error);
   });
-});
+});*/
 
 // Listen for real time updates
 function ListenForUpdates(){
@@ -90,8 +91,94 @@ function ListenForUpdates(){
 
   query.onSnapshot(function(snapshot) {
       snapshot.forEach(doc => {
-        console.log(doc.id);
+        chat_history.push(doc.id);
     });
+      console.log(total_chats, chat_history.length);
+      if(total_chats < chat_history.length){
+        AddDiv();      // Add div for new contact in chat-sidebar 
+        RefreshUI();  // Refresh chats of the users contacted and listen for updates
+      }
+      else{
+        RefreshUI();  // Refresh chats of the users contacted and listen for updates
+      }
   });
 }
 
+function RefreshUI(){
+  var count=0; 
+  //console.log(chat_history);
+  var text= '';
+  total_chats= chat_history.length;
+  chat_history.forEach(item=>{
+    count=count+1;
+    var chat_content= document.querySelector("#chatbox"+count.toString()+" .chat-content");
+    var query= db.collection("messages").doc(item).collection("chat").orderBy('timestamp');
+     query.get().then(function(snapshot) {
+      snapshot.forEach(doc => {
+        var data= doc.data();
+        var current='';
+          if(data.sender == cur_email){
+            current='<div class="chat">'+
+                '<div class="chat-user">'+
+                  '<a class="avatar m-0">'+
+                    '<img src="images/user/1.jpg" alt="avatar" class="avatar-35 ">'+
+                  '</a>'+
+                    '<span class="chat-time mt-1">6:45</span>'+
+                '</div>'+
+                '<div class="chat-detail">'+
+                  '<div class="chat-message">'+
+                    '<p class="msg-text">'+data.msg+'</p>'+
+                  '</div>'+
+                '</div>'+
+              '</div>';
+        }
+        else{
+          current='<div class="chat chat-left">'+
+                '<div class="chat-user">'+
+                  '<a class="avatar m-0">'+
+                    '<img src="images/user/1.jpg" alt="avatar" class="avatar-35 ">'+
+                  '</a>'+
+                    '<span class="chat-time mt-1">6:45</span>'+
+                '</div>'+
+                '<div class="chat-detail">'+
+                  '<div class="chat-message">'+
+                    '<p class="msg-text">'+data.msg+'</p>'+
+                  '</div>'+
+                '</div>'+
+              '</div>';
+        }        
+        text= text+current;
+      });
+      chat_content.innerHTML= text;
+      text='';
+    });
+  });
+  chat_history=[];
+}
+
+function AddDiv(){
+  var chat_sidebar = document.querySelector("#chat-sidebar");
+  var all_chats= document.querySelector('#all_chats');
+  var text= '';
+  var chat_content= '';
+  var count= 0;
+  chat_history.forEach(item=>{
+    count= count+1;
+    var current= '<li>'+
+               '<a role="tab" data-toggle="pill" href="#chatbox'+count.toString()+'">'+
+                  '<div class="d-flex align-items-center">'+
+                     '<div class="avatar mr-3">'+
+                        '<img src="images/user/07.jpg" alt="chatuserimage" class="avatar-50 ">'+
+                        '<span class="avatar-status"><i class="ri-checkbox-blank-circle-fill text-warning"></i></span>'+
+                     '</div>'+
+                     '<div class="chat-sidebar-name">'+
+                        '<h6 class="mb-0">'+item+'</h6>'+
+                        '<span>There are many </span>'+
+                     '</div>'+
+                  '</div>'+
+                '</a>'+
+             '</li>';
+    text= text+current;
+  });
+  chat_sidebar.innerHTML= text;
+}
