8dbc8d084d6fbb6c5fc00a2a326f6852a3123304
Hemal Gokhale <31528745+gokhalehemal11@users.noreply.github.com>
2020-10-11 10:51:56 +0530

    Update login.js

--- scripts/login.js
+++ scripts/login.js
@@ -1,17 +1,6 @@
 var email="";
 var dispname="";
 
-// Liten for the change in Auth state of user. Signed In, Signed Out or Newly Signed Up.
-auth.onAuthStateChanged(user => {
-  if (user) {         // If a user signed up or signed in
-    UpdateStatus(user.email);   // User sign in and update status
-    console.log('user logged in: ', user);
-  } else {
-    console.log('user logged out');
-    //window.location.replace("chat.html");
-  }
-});
-
 // User Sign-in 
 const loginForm = document.querySelector('#login');
 loginForm.addEventListener('submit', (e) => {
@@ -25,17 +14,32 @@ loginForm.addEventListener('submit', (e) => {
   // Successful login shall reflect in onAuthStateChanged(). 
   auth.signInWithEmailAndPassword(email, password).then(cred => {
     loginForm.reset();
+    db.collection('users').doc(email).update({
+    status: true,
+    }).then(function(){
+      window.location.replace("ui-chat.html");
+    }).catch(function(error) {
+      console.error('Error writing to database', error);
+    });
   });
 });
 
-// Update active status of user after login
-function UpdateStatus(docname){
-  //console.log(docname);
-  db.collection('users').doc(docname).update({
-    status: true,
-  }).then(function(){
-    window.location.replace("index.html");
-  }).catch(function(error) {
-    console.error('Error writing to database', error);
+const google_sign_in= document.querySelector("#google-sign-in");
+google_sign_in.addEventListener('click', (e) =>{
+  e.preventDefault();
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider).then(cred =>{
+    //console.log(cred.user);
+    db.collection('users').doc(cred.user.email).set({
+      name: cred.user.displayName,
+      photo: cred.user.photoURL,
+      status: true
+    } ,  {
+        merge: true
+      }).then(function(){
+        window.location.replace("ui-chat.html");
+      }).catch(function(error) {
+      console.error('Error writing to database', error);
+    });
   });
-}
+});
